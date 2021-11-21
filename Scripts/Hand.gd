3299d17f47728ffffdab42b2f631975665e5409b
Jack McGuinness <>
2021-11-21 00:47:12 +0000

    Main refactor of CardSlot functionality
    Communication between card slot is now dealt with exclusively within the CardSlot.gd script, with instances of CardSlots communicating directly. Exception for this CardSlot IDs being populated by Hand.gd on ready.
    
    Drag and drop from hand to gameboard now works, but the hand slot is emptied in both hands.
    TODO: make different instances of Hand scene fill and empty individually.

--- Scripts/Hand.gd
+++ Scripts/Hand.gd
@@ -8,11 +8,16 @@ var slot_usage = {
 	"Slot 5": "0",
 }
 
+func _process(delta: float) -> void:
+	
+	if is_connected("test_signal", self, "_on_CardSlot_test_signal"):
+		print("connected")
+
 func _ready():
 	update_hand_slots("bsquall", 3)
 	update_hand_slots("rsquall", 1)
 	update_hand_slots("bsquall", 4)
-
+	update_slot_ids()
 
 func update_hand_slots(var card_id, var slot_num):
 	var key = "Slot " + str(slot_num)
@@ -21,18 +26,22 @@ func update_hand_slots(var card_id, var slot_num):
 
 func update_slot_textures():
 	for n in slot_usage.values().size():
-		
+
 		var slot_number = n + 1
 		var key = "Slot " + str(slot_number)
-		
+
 		var node_name = "CardSlot" + str(slot_number)
 		var new_card_texture_filepath = "res://Resources/Cards/" + slot_usage[key] + ".jpg"
-		
+
 		var card_texture_node = get_node(node_name).get_node("CardTexture")
-		
+
 		card_texture_node.texture = load(new_card_texture_filepath)
 		
-		print(card_texture_node.name)
+
+func update_slot_ids():
+	for n in get_children():
+		var key = n.name.replace("CardSlot", "Slot ")
+		n.slot_id = slot_usage[key]
 
 func get_slot_usage():
 	return slot_usage
